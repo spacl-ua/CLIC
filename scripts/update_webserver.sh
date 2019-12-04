cea95a31eea1ac962b4698a84a970027e87f149b
Lucas Theis <lucas@theis.io>
2019-12-04 10:30:11 +0000

    Remove redundant script

--- scripts/update_webserver.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-IP_ADDRESS=$(gcloud compute addresses describe clic2020-web --region us-west1 --format 'value(address)')
-DIGEST=$(gcloud container images describe --format 'get(image_summary.digest)' gcr.io/clic-215616/web)
-cat web/web.yaml | sed "s/{{ IP_ADDRESS }}/${IP_ADDRESS}/g" | sed "s/{{ DIGEST }}/${DIGEST}/g" | kubectl apply -f -
