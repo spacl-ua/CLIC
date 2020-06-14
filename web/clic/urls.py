78687842733dbfd3163fed9a3c9cf6e0a3ed81aa
Lucas Theis <lucas@theis.io>
2020-06-14 16:15:19 +0200

    Redirect /leaderboard to test leaderboard

--- web/clic/urls.py
+++ web/clic/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
 	path('submission/<pk>/', views.submission),
 	path('submissions/', views.submissions_list, name='submissions'),
 	path('leaderboard/<task>/<phase>/', views.leaderboard, name='leaderboard'),
-	path('leaderboard/', RedirectView.as_view(url='/leaderboard/lowrate/valid/', permanent=False)),
+	path('leaderboard/', RedirectView.as_view(url='/leaderboard/lowrate/test/', permanent=False)),
 	path('schedule/<pk>/', views.schedule, name='schedule'),
 	path('publications/', include('publications.urls'), name='publications'),
 ]
